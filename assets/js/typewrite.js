f936e60183835521305cad42220dd32e09a5ab55
Elena Gonzalez <elenaglezrabal@gmail.com>
2022-01-18 20:46:44 +0100

    cover greeting

--- assets/js/typewrite.js
+++ assets/js/typewrite.js
@@ -12,26 +12,39 @@ const carouselText = [
 async function typeSentence(sentence, eleRef, delay = 120) {
   const letters = sentence.split("");
   let i = 0;
-  while (i < letters.length) {
+  while(i < letters.length) {
     await waitForMs(delay);
     $(eleRef).append(letters[i]);
-    i++;
+    i++
   }
   return;
 }
 
-async function carousel(carouselList, eleRef) {
-  for (var i = 0; carouselList[i]; i++) {
-    await typeSentence(carouselList[i].text, eleRef);
-    await waitForMs(1000);
-    if (i >= carouselList.length) {
-      i = 0;
-    }
+async function deleteSentence(eleRef) {
+  const sentence = $(eleRef).html();
+  const letters = sentence.split("");
+  let i = 0;
+  while(letters.length > 0) {
+    await waitForMs(100);
+    letters.pop();
+    $(eleRef).html(letters.join(""));
   }
 }
 
+async function carousel(carouselList, eleRef) {
+    var i = 0;
+    while(true) {
+      await typeSentence(carouselList[i].text, eleRef);
+      await waitForMs(1500);
+      await deleteSentence(eleRef);
+      await waitForMs(500);
+      i++
+      if(i >= carouselList.length) {i = 0;}
+    }
+}
+
 function waitForMs(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 carousel(carouselText, "#sentence")
